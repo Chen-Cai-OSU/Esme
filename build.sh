70cb374b09d1271334418f5e0968b42dd4fbd5e5
Chen-Cai-OSU <chencai+1@gmail.com>
2019-05-19 21:09:18 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+python=/Users/baidu/anaconda3/bin/python
+file=/Users/baidu/Documents/Esme/Esme/permutation/replicate.py
+
+platform='unknown'
+unamestr=`uname`
+echo $unamestr
+
+if [[ "$unamestr" == 'Linux' ]]; then
+    /home/cai.507/anaconda3/bin/python setup.py sdist bdist_wheel
+    /home/cai.507/anaconda3/bin/pip install .
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    sudo $python setup.py install
+    sudo $python $file
+fi
+
+
+
